89da80e52a3b185adfae1c350b2181293c51adc7
RumyantsevMI <Jordas5@mail.ru>
2022-08-18 10:44:55 +0300

    Создание прототипа таблицы

--- src/main.js
+++ src/main.js
@@ -1,10 +1,10 @@
 //Код для переключения между вкладками на главной области программы.
 const tabs = Array.from(document.querySelectorAll(".tabs"));
-for (let b = 0; b < tabs.length; b++){
+for (let b = 0; b < tabs.length; b++) {
   const tab_content = Array.from(tabs[b].querySelectorAll(".tab__content"));
   const tab = Array.from(tabs[b].querySelectorAll(".tab"));
   for (let i = 0; i < tab.length; i++) {
-    tab[i].addEventListener("click", getActive) 
+    tab[i].addEventListener("click", getActive)
     function getActive() {
       let curentIndex = tab.findIndex(element => element.classList.contains("tab_active"));
       tab[curentIndex].classList.remove("tab_active");
@@ -15,14 +15,14 @@ for (let b = 0; b < tabs.length; b++){
   }
 }
 
-//Аналогичный код для внутренних вкладок
+//Аналогичный код для внутренних вкладок.
 
 const untabs = Array.from(document.querySelectorAll(".untabs"));
-for (let b = 0; b < untabs.length; b++){
+for (let b = 0; b < untabs.length; b++) {
   const untab_content = Array.from(untabs[b].querySelectorAll(".untab__content"));
   const untab = Array.from(untabs[b].querySelectorAll(".untab"));
   for (let i = 0; i < untab.length; i++) {
-    untab[i].addEventListener("click", getUnActive) 
+    untab[i].addEventListener("click", getUnActive)
     function getUnActive() {
       let curentIndex = untab.findIndex(element => element.classList.contains("untab_active"));
       untab[curentIndex].classList.remove("untab_active");
@@ -33,28 +33,42 @@ for (let b = 0; b < untabs.length; b++){
   }
 }
 
-//Код для кнопок боковой панели 
+//Код для кнопок боковой панели.
 
+
+//Код для кнопки перехода к списку ошибок.
 const alarmButton = document.querySelector(".alarm-button");
 alarmButton.addEventListener("click", toAlarmList);
 function toAlarmList() {
   const tab = Array.from(document.querySelectorAll(".tab"));
-  tab.forEach(tab => { tab.classList.remove("tab_active")
+  tab.forEach(tab => {
+    tab.classList.remove("tab_active")
   });
   const infoPage = document.querySelector(".info-page");
   infoPage.classList.add("tab_active");
   const tabContent = Array.from(document.querySelectorAll(".tab__content"));
   const infoContent = document.querySelector(".tab__content_info");
-  tabContent.forEach(tabContent => {tabContent.classList.remove("tab__content_active")});
+  tabContent.forEach(tabContent => { tabContent.classList.remove("tab__content_active") });
   infoContent.classList.add("tab__content_active");
   const untab = Array.from(infoContent.querySelectorAll(".untab"));
-  untab.forEach(untab => {untab.classList.remove("untab_active")
+  untab.forEach(untab => {
+    untab.classList.remove("untab_active")
   });
   const alarmList = infoContent.querySelector(".alarm-statistics");
   alarmList.classList.add("untab_active");
   const untabContent = Array.from(infoContent.querySelectorAll(".untab__content"));
-  untabContent.forEach(untabContent => {untabContent.classList.remove("untab__content_active")
+  untabContent.forEach(untabContent => {
+    untabContent.classList.remove("untab__content_active")
   });
   const alarmListContent = infoContent.querySelector(".alarm-statistics-content");
   alarmListContent.classList.add("untab__content_active")
 }
+
+// Отображение реального времени на экране приложения.
+let time = setInterval(function () {
+  let date = new Date();
+  let hours = (date.getHours() < 10) ? '0' + date.getHours() : date.getHours();
+  let minutes = (date.getMinutes() < 10) ? '0' + date.getMinutes() : date.getMinutes();
+  let seconds = (date.getSeconds() < 10) ? '0' + date.getSeconds() : date.getSeconds();
+  document.querySelector('.time').innerHTML = hours + ':' + minutes + ':' + seconds;
+}, 1000);
