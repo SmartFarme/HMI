67dfba52e492eeb38a7f1577d832608be4005ad6
RumyantsevMI <Jordas5@mail.ru>
2022-08-29 18:05:46 +0300

    Изменение логотипа, фикс углов и линий, подгонка изображений без сглаживания

--- src/main.js
+++ src/main.js
@@ -72,3 +72,22 @@ let time = setInterval(function () {
   let seconds = (date.getSeconds() < 10) ? '0' + date.getSeconds() : date.getSeconds();
   document.querySelector('.time').innerHTML = hours + ':' + minutes + ':' + seconds;
 }, 1000);
+
+
+const dropdown = document.querySelectorAll(".dropdown");
+const list = document.querySelectorAll(".dropdown__list");
+const value = document.querySelectorAll(".dropdown__value");
+for (let i = 0; i < dropdown.length; i++) {
+  value[i].addEventListener("click", onClick);
+  function onClick() {
+    list[i].classList.toggle("dropdown__list_active");
+  }
+  const item = dropdown[i].querySelectorAll(".dropdown__item");
+  for (let b = 0; b < item.length; b++) {
+    item[b].addEventListener("click", function (event) {
+      value[i].textContent = item[b].textContent;
+      list[i].classList.remove("dropdown__list_active");
+      event.preventDefault();
+    });
+  }
+}
