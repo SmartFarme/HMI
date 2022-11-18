ca2a3c00809e9ad3f6c5a255f45b9bbe3e5ed4a8
SheykinMO <maska575@yandex.ru>
2022-11-18 18:12:26 +0300

    Использование npm-модулей заместо online версий

--- src/index.js
+++ src/index.js
@@ -17,9 +17,13 @@ const createWindow = () => {
     frame: false,
     fullscreen: false,
     autoHideMenuBar: true,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false
+    }
   });
 
-  mainWindow.webContents.openDevTools();
+  //mainWindow.webContents.openDevTools();
   // and load the index.html of the app.
   mainWindow.loadURL(`file://${__dirname}/index.html`);
 
