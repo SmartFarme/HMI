3e0c8e3be5ddf4a9b501453bf2e4edc21b2605d6
RumyantsevMI <Jordas5@mail.ru>
2022-12-06 09:57:09 +0300

    Редактирование кнопок fix-4

--- src/main.js
+++ src/main.js
@@ -1,19 +1,5 @@
 /// Код для переключения между вкладками на главной области программы. ///
-const tabs = Array.from(document.querySelectorAll(".tabs"));
-for (let b = 0; b < tabs.length; b++) {
-  const tab_content = Array.from(tabs[b].querySelectorAll(".tab__content"));
-  const tab = Array.from(tabs[b].querySelectorAll(".tab"));
-  for (let i = 0; i < tab.length; i++) {
-    tab[i].addEventListener("click", getActive)
-    function getActive() {
-      let curentIndex = tab.findIndex(element => element.classList.contains("tab_active"));
-      tab[curentIndex].classList.remove("tab_active");
-      tab_content[curentIndex].classList.remove("tab__content_active");
-      tab[i].classList.add("tab_active");
-      tab_content[i].classList.add("tab__content_active");
-    }
-  }
-}
+
 
 /// Код для переключения между подвкладками. ///
 
